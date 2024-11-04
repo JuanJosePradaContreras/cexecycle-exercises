ed943cf9e53c742677ab6f64f96f3e936fc68c58
JuanJosePradaContreras <180313522+JuanJosePradaContreras@users.noreply.github.com>
2024-11-03 23:03:36 -0500

    Ejercicio 2, Potencias

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-#Ejercicio 1
+#Ejercicio 2
 
-numero = int(input("Ingrese un número: "))
+#Escriba un programa que genere todas las potencias de 2, desde la 0-ésima hasta la 
+#ingresada por el usuario:
 
-for i in range(1, 11):
-    resultado = numero * i
-    print(f"{numero} x {i} = {resultado}")
+
+def potencias_de_dos(n):
+    for i in range(n + 1):
+        print(f"2^{i} = {2 ** i}")
+
+def main():
+    # Solicitar la entrada al usuario
+    n = int(input("Ingrese el valor de n (potencia máxima): "))
+    
+    # Generar y mostrar las potencias de 2
+    potencias_de_dos(n)
+
+# Llama a la función principal
+main()
